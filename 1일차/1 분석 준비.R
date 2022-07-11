661e8f64b008e4d99a626dcbf27d05e8b59116e0
ahora3734 <ahora3734@gmail.com>
2022-07-12 01:05:46 +0900

    2022.07.12_first

---
+++ 1일차/1 분석 준비.R
@@ -0,0 +1,9 @@
+library(readxl)
+data1 <- read_excel(path="data.xlsx", sheet=1)
+
+data1 <- read_excel("data.xlsx", 1)
+data1 <- read_excel("data.xlsx")
+
+data1 <- readxl::read_excel("data.xlsx")
+
+install.packages("languageserversetup")
---
+++ 1일차/2 기술통계.R
@@ -0,0 +1,12 @@
+#### 1. 데이터 로딩 ####
+library(readxl)                     # 엑셀 데이터를 불러올 수 있는 패키지 로딩
+data1 <- read_excel("data.xlsx")    # 엑셀 데이터 불러오기
+
+
+#### 2. 기술통계 ####
+library(dlookr)                     # 기술통계 쉽게 내주는 패키지 로딩
+diagnose_category(data1)            # 범주형 데이터 통계
+describe(data1)                     # 연속형 데이터 통계
+
+table(data1$성별, data1$연령)
+table(data1$성별, data1$선호도)
